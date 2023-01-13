62193c51cee66aae602f6f2af44db7b6e0cad55d
sindhu2192 <sindhu.ravulapally@gmail.com>
2023-01-13 09:58:18 -0800

    13/1/23 created courses dynamically

--- grails-app/controllers/com/pages/CoursesController.groovy
+++ grails-app/controllers/com/pages/CoursesController.groovy
@@ -1,6 +1,14 @@
 package com.pages
 
+import org.management.CourseService
+
 class CoursesController {
 
-    def index() { }
+    CourseService courseService
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond courseService.list(params), model:[courseCount: courseService.count()]
+    }
+
 }
